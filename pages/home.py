5961909b062b0fe783ea93359e07e1bf10c27c78
Flavie Laurent <flavie1002@gmail.com>
2026-09-17 16:59:51 +0200

    add homepage_nv2

--- pages/home.py
+++ pages/home.py
@@ -4,13 +4,13 @@ register_page(__name__, path='/', name='Home')
 
 layout = html.Div([
 
-    html.P('Bienvenue sur notre site de Fan de canards'),
+    html.H1('Bienvenue sur notre site de Fan de canards'),
     html.P("Elle est composé en pages sur les plats autour du canards ainsi que sur les caractéristiques biologique du canard"),
     
-    html.H1("Le canard cet animal si commun"),
+    html.H3("Le canard cet animal si commun"),
     html.P("Que ça soit un colver ou un canard de barbarie, les canards nous entourent au quotidien."),
 
-    html.H2("Mais pourtant si bon"),
+    html.H3("Mais pourtant si bon"),
     html.P("De nombreux plats régionaux sont basés sur les canards. Notamment dans le Gers où le magret de canards, les gesiers ou bien le foie gras est un vrai incontournable."),
 
 
